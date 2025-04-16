a24ae8963c87aab5d11ed68990c7ce59e977f777
vsn4ke <vsn4ke@gmail.com>
2025-04-16 21:14:23 +0200

    fixed unwrap in Rng

--- src/utils/rng.rs
+++ src/utils/rng.rs
@@ -7,36 +7,31 @@ use std::{
 #[derive(Clone, Copy)]
 pub struct Rng {
     master_seed: u64,
-    //state_32: i32,
     state_64: u64,
 }
 
 impl Rng {
-    pub fn new(seed: u64) -> Rng {
-        Rng {
+    pub fn new(seed: u64) -> Self {
+        Self {
             master_seed: seed,
             state_64: seed,
         }
     }
 
-    pub fn get_seed(self) -> u64 {
-        self.master_seed
-    }
-
-    pub fn random_seed() -> Rng {
+    pub fn random_seed() -> Self {
         let start = SystemTime::now();
-        let seed = start
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backward")
-            .as_millis() as u64;
-        Rng::new(seed)
+        let seed = match start.duration_since(UNIX_EPOCH) {
+            Ok(d) => d.as_millis() as u64,
+            Err(_) => hash_string("Time went backward"),
+        };
+
+        Self::new(seed)
     }
 
     pub fn from_string(string: impl Into<String>) -> Self {
         let s: String = string.into();
-        let mut hasher = DefaultHasher::new();
-        s.hash(&mut hasher);
-        Self::new(hasher.finish())
+
+        Self::new(hash_string(s))
     }
 
     fn next_state_64(&mut self) -> u64 {
@@ -50,6 +45,10 @@ impl Rng {
         self.random_range_from_seed(self.next_state_64(), range)
     }
 
+    pub fn get_seed(self) -> u64 {
+        self.master_seed
+    }
+
     pub fn get_seed_from_xy(self, current_seed: u64, x: i32, y: i32) -> u64 {
         let x = x as u64;
         let y = y as u64;
@@ -61,4 +60,10 @@ impl Rng {
         let modulo = range.end - range.start;
         (seed as i32).abs() % modulo + range.start
     }
+}
+
+fn hash_string(string: impl Into<String>) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    string.into().hash(&mut hasher);
+    hasher.finish()
 }
